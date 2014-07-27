28d530f7a336dbe7352f9e3d360c7142f443647b
Pankaj Daga <p.daga@ucl.ac.uk>
2014-07-27 09:10:26 +0100

    sum of square difference

---
+++ src/measures/ssd.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def compute_cost(target, warped, mask):
+
+    """
+    Compute the sum of square difference between two images.
+
+    :rtype : float
+    :param target: The target image
+    :param warped: The warped image. Should be in the same space as the target
+    :param mask:
+    :return:
+    """
+    warped_data = warped.data.astype(np.float32)
+    target_data = target.data.astype(np.float32)
+    masked_data = mask.data > 0
+    ssd = np.sum(((warped_data - target_data)**2)*masked_data)
+    return ssd
