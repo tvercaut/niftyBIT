Corrected mistakes in the computation of SSD. Mask image was not used correctly

src/measures/ssd.py
@@ -9,11 +9,11 @@ def compute_ssd(target, warped, mask):
     :rtype : float
     :param target: The target image
     :param warped: The warped image. Should be in the same space as the target
-    :param mask:
+    :param mask: The image mask.
     :return: The computed sum of square difference
     """
     warped_data = warped.data.astype(np.float32)
     target_data = target.data.astype(np.float32)
-    masked_data = mask.data > 0
-    ssd = np.sum(((warped_data - target_data)**2)*masked_data)
+    valid_voxels = np.count_nonzero(mask.data)
+    ssd = np.sum(((warped_data - target_data)**2))/valid_voxels
     return ssd