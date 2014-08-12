5731d736d727a76d74b12c1242051541915ecfc6
Pankaj Daga <p.daga@ucl.ac.uk>
2014-08-12 23:32:46 +0100

    Use 64 bit for SSD computation

--- src/measures/ssd.py
+++ src/measures/ssd.py
@@ -12,8 +12,8 @@ def compute_ssd(target, warped, mask):
     :param mask: The image mask.
     :return: The computed sum of square difference
     """
-    warped_data = warped.data.astype(np.float32)
-    target_data = target.data.astype(np.float32)
+    warped_data = warped.data.astype(np.float64)
+    target_data = target.data.astype(np.float64)
     valid_voxels = np.count_nonzero(mask.data)
     ssd = np.sum(((warped_data - target_data)**2))/valid_voxels
     return ssd
