9b4a0e27f6d10c7ef080438a94bf148c55c82fcc
Pankaj Daga <p.daga@ucl.ac.uk>
2014-11-06 15:31:27 +0000

    Generate random deformation fields

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,11 +1,15 @@
+__author__ = 'Pankaj Daga'
+
 import numpy as np
 from utils.image import Image
+from scipy import ndimage
 
 
 class RegError(Exception):
     """
     Exception class override
     """
+
     def __init__(self, v):
         super(RegError, self).__init__(v)
         self.message = v
@@ -33,16 +37,94 @@ def generate_identity_deformation(def_image, image=None):
     voxels = np.mgrid[[slice(i) for i in vol_ext]]
     voxels = [d.reshape(vol_ext, order='F') for d in voxels]
     mms = [voxel_2_xyz[i][3] + sum(voxel_2_xyz[i][k] * voxels[k]
-           for k in range(len(voxels)))
+                                   for k in range(len(voxels)))
            for i in range(len(voxel_2_xyz) - (4 - len(vol_ext)))]
 
     data = np.squeeze(def_image.data)
+    mms = np.squeeze(mms)
+
     for i in range(data.shape[-1]):
         data[..., i] = mms[i]
 
 
-def field_conversion_method(image, field_image, is_deformation=True):
+def generate_random_smooth_deformation(volume_size,
+                                       max_deformation=3,
+                                       sigma=1):
+    """
+    Generate a random smooth deformation
+    param: def_image: Deformation field image. It will be updated with the deformation.
+    max_deformation_in_voxels: Maximum amount of deformation in voxels.
 
+    The method ensures that the jacobian determinant of the deformation is positive.
+    """
+    if sigma <= 0:
+        sigma = max_deformation/3
+
+    if len(volume_size) > 3:
+        volume_size = volume_size[0:3]
+
+    dims = list()
+    dims.extend(volume_size)
+    while len(dims) < 4:
+        dims.extend([1])
+    dims.extend([len(volume_size)])
+
+    # Inititalise with zero
+    data = np.zeros(dims, dtype=np.float32)
+    def_field = Image.generate_default_image_from_data(data)
+    generate_identity_deformation(def_field)
+    # Generate a random displacement field
+    displacement = max_deformation * 2 * \
+        (np.random.random_sample(def_field.data.shape) - 0.5)
+    # Smooth the displacement field
+    displacement = ndimage.gaussian_filter(displacement, sigma=sigma)
+
+    disp_s = def_field.data.squeeze() + displacement.squeeze()
+    done = False
+    while not done:
+        if len(volume_size) == 2:
+            grad = np.gradient(disp_s[..., 0])
+            x_x = grad[0]
+            x_y = grad[1]
+            grad = np.gradient(disp_s[..., 1])
+            y_x = grad[0]
+            y_y = grad[1]
+
+            jac_det = x_x * y_y - x_y * y_x
+
+            if np.min(jac_det) < 0.1:
+                displacement = ndimage.gaussian_filter(displacement, sigma=sigma)
+                disp_s = def_field.data.squeeze() + displacement.squeeze()
+            else:
+                done = True
+        else:
+            grad = np.gradient(disp_s[..., 0])
+            x_x = grad[0]
+            x_y = grad[1]
+            x_z = grad[2]
+            grad = np.gradient(disp_s[..., 1])
+            y_x = grad[0]
+            y_y = grad[1]
+            y_z = grad[2]
+            grad = np.gradient(disp_s[..., 2])
+            z_x = grad[0]
+            z_y = grad[1]
+            z_z = grad[2]
+
+            jac_det = x_x * (y_y*z_z - y_z*z_y) - \
+                x_y * (y_x*z_z - y_z*z_x) + x_z * (y_x*z_y - y_y*z_x)
+
+            if np.min(jac_det) < 0.1:
+                displacement = ndimage.gaussian_filter(displacement, sigma=sigma)
+                disp_s = def_field.data.squeeze() + displacement.squeeze()
+            else:
+                done = True
+
+    def_field.data += displacement
+    return def_field
+
+
+def field_conversion_method(image, field_image, is_deformation=True):
     data = np.zeros_like(field_image.data, dtype=np.float32)
     field = Image.from_data(data, image.get_header())
     # Matrix to go from voxel to world space
@@ -51,11 +133,12 @@ def field_conversion_method(image, field_image, is_deformation=True):
     voxels = np.mgrid[[slice(i) for i in vol_ext]]
     voxels = [d.reshape(vol_ext, order='F') for d in voxels]
     mms = [voxel_2_xyz[i][3] + sum(voxel_2_xyz[i][k] * voxels[k]
-           for k in range(len(voxels)))
+                                   for k in range(len(voxels)))
            for i in range(len(voxel_2_xyz) - (4 - len(vol_ext)))]
 
     input_data = np.squeeze(field_image.data)
     field_data = np.squeeze(data)
+    mms = np.squeeze(mms)
     if is_deformation:
         for i in range(data.shape[-1]):
             # Output is the displacement field
@@ -147,40 +230,46 @@ def compute_variance(array):
 
 
 def initialise_field(im, affine=None):
-        """
-        Create a field image from the specified target image.
-        Sets the data to 0.
+    """
+    Create a field image from the specified target image.
+    Sets the data to 0.
 
-        Parameters:
-        -----------
-        :param im: The target image. Mandatory.
-        :param affine: The initial affine transformation
-        :return: Return the created field object
-        """
-        vol_ext = im.vol_ext
-        dims = list()
-        dims.extend(vol_ext)
-        while len(dims) < 4:
-            dims.extend([1])
-        dims.extend([len(vol_ext)])
+    Parameters:
+    -----------
+    :param im: The target image. Mandatory.
+    :param affine: The initial affine transformation
+    :return: Return the created field object
+    """
+    vol_ext = im.vol_ext
+    dims = list()
+    dims.extend(vol_ext)
+    while len(dims) < 4:
+        dims.extend([1])
+    dims.extend([len(vol_ext)])
 
-        # Inititalise with zero
-        data = np.zeros(dims, dtype=np.float32)
-        field = Image.from_data(data, im.get_header())
+    # Inititalise with zero
+    data = np.zeros(dims, dtype=np.float32)
+    field = Image.from_data(data, im.get_header())
 
-        # We have supplied an affine transformation
-        if affine is not None:
-            if affine.shape != (4, 4):
-                raise RegError('Input affine transformation '
-                               'should be a 4x4 matrix.')
-            # The updated transformation
-            transform = affine * im.voxel_2_mm
-            field.update_transformation(transform)
+    # We have supplied an affine transformation
+    if affine is not None:
+        if affine.shape != (4, 4):
+            raise RegError('Input affine transformation '
+                           'should be a 4x4 matrix.')
+        # The updated transformation
+        transform = affine * im.voxel_2_mm
+        field.update_transformation(transform)
 
-        return field
+    return field
 
 
 def ndmesh(*xi, **kwargs):
+    """
+    n-dimensional mesh code stripped from numpy. This ensures meshgrid can
+    be called from older versions of numpy which only supported 2-D meshgrid
+    calls.
+    """
+
     if len(xi) < 2:
         msg = 'meshgrid() takes 2 or more arguments (%d given)' % int(len(xi) > 0)
         raise ValueError(msg)
@@ -200,4 +289,42 @@ def ndmesh(*xi, **kwargs):
         return [x * mult_fact for x in output]
     else:
         return np.broadcast_arrays(*output)
+
+
+def compute_spatial_gradient(image, derivative=None):
+    """
+    Compute the spatial gradient of the image using finite differences
+    :param image: The image whose gradient we need to compute
+    :param derivative: The derivative image. If it is none, it is allocated
+    """
+
+    if derivative is None:
+        derivative = initialise_field(image)
+
+    transform = image.voxel_2_mm
+    dims = []
+    for i in range(len(image.vol_ext)):
+        dims.append(transform[i, i])
+
+    grad = np.gradient(image.data, *dims)
+    output_data = derivative.data.squeeze()
+    for i in range(derivative.data.shape[-1]):
+        output_data[..., i] = grad[i].squeeze()
+
+    return derivative
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
