966e541f25a929e28b8e88ff32b933ca1a119f47
Pankaj Daga <p.daga@ucl.ac.uk>
2014-11-18 16:17:15 +0000

    This fixes issues #3 and implements the enhancement suggested by Tom V in #2

--- src/utils/resampler.py
+++ src/utils/resampler.py
@@ -94,27 +94,20 @@ class PositionFieldComposer(object):
         :return The composed position field
         """
 
-        d = np.zeros(left.data.shape)
-        result = Image.from_data(d, header=left.get_header())
-
-        vol_ext = left.vol_ext[:left.data.shape[-1]]
-        left_data = [left.data[..., i].reshape(vol_ext, order='F')
-                     for i in range(left.data.shape[-1])]
+        d = np.zeros(right.data.shape)
+        result = Image.from_data(d, header=right.get_header())
+        vol_ext = right.vol_ext[:right.data.shape[-1]]
+        right_data = [right.data[..., i].reshape(vol_ext, order='F')
+                      for i in range(right.data.shape[-1])]
 
         data = np.squeeze(result.data)
 
         for i in range(data.shape[-1]):
-            # todo: Incorrect behaviour at boundary
-            # This is an issue we need to revisit.
-            # Nearest mode is not good when we are dealing with fields.
-            # Unfortunately map_coordinates does not allow for leaving the value
-            # as is and we will need to implement this at some point.
-            ndimage.map_coordinates(np.squeeze(right.data[..., i]),
-                                    left_data,
+            ndimage.map_coordinates(np.squeeze(left.data[..., i]),
+                                    right_data,
                                     data[..., i],
                                     mode='nearest',
                                     order=self.order, prefilter=True)
-
         return result
 
 
@@ -133,16 +126,13 @@ class DisplacementFieldComposer(object):
         -----------
         :param left: Outer displacement field.
         :param right: Inner displacement field.
-        :param result: Resulting position field after composition.
-        If it is none, it will be allocated
         Order of composition: left(right(x))
+        :return Return the composed displacement field
         """
-        left_def_image = generate_position_from_displacement(left)
         right_def_image = generate_position_from_displacement(right)
-
         composer = PositionFieldComposer()
-        result = composer.compose(left_def_image, right_def_image)
-        result = generate_displacement_from_deformation(result)
+        result = composer.compose(left, right_def_image)
+        result.data += right.data
         return result
 
 
