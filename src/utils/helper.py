2a33623b55716ac8e03c5aa6e4ad7623199a4972
Tom Vercauteren <t.vercauteren@ucl.ac.uk>
2014-12-03 20:17:07 +0000

    nifti stores matrix valued images as vector valued images using row-major ordering. reflect this in code for issue #6

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -281,15 +281,14 @@ def initialise_jacobian_field(im, affine=None):
     -----------
     :param im: The target image/field. Mandatory.
     :param affine: The initial affine transformation
-    :return: Return the created jacobian field object
+    :return: Return the created jacobian field object. Each jacobian is stored in a vector of size 9 in row major order
     """
     vol_ext = np.array(im.vol_ext)
     dims = list()
     dims.extend(vol_ext)
     while len(dims) < 4:
         dims.extend([1])
-    dims.extend([len(vol_ext[vol_ext>1])])
-    dims.extend([len(vol_ext[vol_ext>1])])
+    dims.extend([len(vol_ext[vol_ext>1])**2])
 
     # Inititalise with zero
     data = np.zeros(dims, dtype=np.float32)
@@ -362,7 +361,7 @@ def compute_jacobian_field(field, jacobian=None):
     """
     Compute the spatial gradient of the field using finite differences
     :param field: The field whose jacobian we need to compute
-    :param jacobian: The jacobian image. If it is none, it is allocated
+    :param jacobian: The jacobian image. If it is none, it is allocated. Each jacobian is stored in a vector of size 9 in row major order
     """
 
     if jacobian is None:
@@ -370,14 +369,15 @@ def compute_jacobian_field(field, jacobian=None):
 
     transform = field.voxel_2_mm
     dims = []
-    for i in range(sum(np.array(field.vol_ext)>1)):
+    num_dims = sum(np.array(field.vol_ext)>1)
+    for i in range(num_dims):
         dims.append(transform[i, i])
 
     output_data = jacobian.data.squeeze()
-    for i in range(jacobian.data.shape[-2]):
+    for i in range(num_dims):
         grad = np.gradient(np.squeeze(field.data[...,i]), *dims)
-        for j in range(jacobian.data.shape[-1]):
-            output_data[..., i, j] = grad[j].squeeze()
+        for j in range(num_dims):
+            output_data[..., i*num_dims+j] = grad[j].squeeze()
 
     return jacobian
 
